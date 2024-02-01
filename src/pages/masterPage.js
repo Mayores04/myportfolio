efba9e9a36d8c918d37e6f8b775339d806eaf862
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-02-01 22:07:26 +0000

    Initial commit

---
+++ src/pages/masterPage.js
@@ -0,0 +1,22 @@
+// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+
+$w.onReady(function () {
+    // Write your JavaScript here
+
+    // To select an element by ID use: $w('#elementID')
+
+    // Click 'Preview' to run your code
+});
+
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function vectorImage4_click(event) {
+    
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
